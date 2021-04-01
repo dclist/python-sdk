9e5345bac5e026b58393c65f13f9fb66702f7d3e
İlker Güzelkokar <guzelkokarilker@gmail.com>
2021-04-01 17:40:13 +0300

    fix!: Changing Bearer token to Sdk version bump to v0.1.5

--- dclist/__init__.py
+++ dclist/__init__.py
@@ -13,11 +13,11 @@ __title__ = 'dclist.py'
 __author__ = 'ilkergzlkkr'
 __copyright__ = 'Copyright 2021-present ilkergzlkkr'
 __license__ = 'MIT'
-__version__ = '0.1.2'
+__version__ = '0.1.5'
 
 from collections import namedtuple
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-version_info = VersionInfo(major=0, minor=1, micro=2, releaselevel='final', serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=5, releaselevel='final', serial=0)
 
 version_info = f'{version_info.major}.{version_info.minor}.{version_info.micro}'
 
